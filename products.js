d175c03bd925db4f7e8673d6d7d7be453ac76b06
khangdao <khangdao2112@gmail.com>
2024-10-04 02:07:21 +0700

    feat: final changes, add start script, fix typo, include docker configuration

--- products.js
+++ products.js
@@ -2,11 +2,10 @@ const products = [
     {
         id: 1,
         image: "thiphamshirt.jpg",
-        name: "Thi Pham Balloon T-Shirt",
+        name: "ThiPham Balloon T-Shirt",
         price: 727,
         description: "地下室には千の子供たちがいます"
     }
 ];
-
 
 module.exports = products;
