46da35ddebf62b363628584be63ae8669d63475c
Geno2806 <quockhanhmini@gmail.com>
2024-10-03 16:27:24 +0700

    THE STORE

---
+++ products.js
@@ -0,0 +1,12 @@
+const products = [
+    {
+        id: 1,
+        image: "ThiPham_shirt.jpg",
+        name: "Thi Pham Balloon T-Shirt",
+        price: 727,
+        description: "地下室には千の子供たちがいます"
+    }
+];
+
+
+module.exports = products;
